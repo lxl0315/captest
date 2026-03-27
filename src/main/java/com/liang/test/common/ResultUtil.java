8defaa27804c6b48174edd3761b00d10799e4a3c
xliang <xuelan.liang@capgemini.com>
2026-03-27 17:17:45 +0800

    11

--- src/main/java/com/liang/test/common/ResultUtil.java
+++ src/main/java/com/liang/test/common/ResultUtil.java
@@ -1,19 +1,11 @@
 package com.liang.test.common;
 
 public class ResultUtil {
-    public static <T> Result<T> success(T data) {
-        Result<T> result = new Result<>();
-        result.setCode(200);
+    public static Result success(Object object) {
+        Result result = new Result();
+        result.setCode(0);
         result.setMessage("success");
-        result.setData(data);
-        return result;
-    }
-
-    public static <T> Result<T> error(String message) {
-        Result<T> result = new Result<>();
-        result.setCode(500);
-        result.setMessage(message);
-        result.setData(null);
+        result.setData(object);
         return result;
     }
 }
